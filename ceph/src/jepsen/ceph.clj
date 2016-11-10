be591f7ff0c6850f096f3464b05dde35747a1d8e
Yiming Zhang <steven.y.zh@hotmail.com>
2016-11-09 17:43:20 -0800

    adding clojure test case - ceph
    - adding a test case to for the first time

---
+++ ceph/src/jepsen/ceph.clj
@@ -0,0 +1,39 @@
+(ns jepsen.ceph
+  (:require [avout.core         :as avout]
+            [clojure.tools.logging :refer :all]
+            [clojure.java.io    :as io]
+            [clojure.string     :as str]
+            [jepsen [db         :as db]
+                    [checker    :as checker]
+                    [client     :as client]
+                    [control    :as c]
+                    [generator  :as gen]
+                    [nemesis    :as nemesis]
+                    [tests      :as tests]
+                    [util       :refer [timeout]]]
+            [jepsen.os.debian   :as debian]
+            [knossos.model      :as model]))
+
+(defn r   [k] (c/exec :ceph :config-key :get k :-o :value :&& :cat :value :&& :echo :" ") )
+(defn w   [k v] (c/exec :ceph :config-key :put k v))
+
+(defn ceph-test
+  [version]
+  (assoc tests/noop-test
+         :name    "ceph"
+         :os      debian/os
+         :db      (db version)
+         :client  (client nil nil)
+         :nemesis (nemesis/partition-random-halves)
+         :generator (->> (gen/mix [r w])
+                         (gen/stagger 1)
+                         (gen/nemesis
+                           (gen/seq (cycle [(gen/sleep 5)
+                                            {:type :info, :f :start}
+                                            (gen/sleep 5)
+                                            {:type :info, :f :stop}])))
+                         (gen/time-limit 60))
+         :model   (model/set)
+         :checker (checker/compose
+                    {:perf   (checker/perf)
+                     :linear checker/linearizable})))
