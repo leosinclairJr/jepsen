6e401a729d3f0825fcb915980c98d6544868671e
Yiming Zhang <steven.y.zh@hotmail.com>
2016-11-14 10:10:39 -0800

    updated the path of ceph.core

--- ceph/test/jepsen/ceph/core_test.clj
+++ ceph/test/jepsen/ceph/core_test.clj
@@ -1,6 +1,7 @@
 (ns ceph.core-test
   (:require [clojure.test :refer :all]
-            [ceph.core :refer :all]))
+            ;;[jepsen.ceph.core :refer :all]))
+            [jepsen.ceph.core :as jc]))
 
 (deftest a-test
   (testing "FIXME, I fail."
