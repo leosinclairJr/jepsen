96793c8d134a689953c133a38f22fdcdfffff1ce
Yiming Zhang <steven.y.zh@hotmail.com>
2016-11-14 09:48:01 -0800

    test: noop-test
    this test is a basis for future work

--- ceph/src/ceph/core.clj
+++ ceph/src/ceph/core.clj
@@ -16,26 +16,27 @@
             [knossos.model      :as model]))
 
 ;;(defn r   [k] (c/exec :ceph :config-key :get k :-o :value :&& :cat :value :&& :echo :" ") )
-(defn r   [k] (c/exec :ceph :config-key :get k) )
-(defn w   [k v] (c/exec :ceph :config-key :put k v))
+;(defn r   [k] (c/exec :ceph :config-key :get k) )
+;(defn w   [k v] (c/exec :ceph :config-key :put k v))
 
 (defn ceph-test
   [version]
-  (assoc tests/noop-test
-         :name    "ceph"
-         :os      debian/os
+  tests/noop-test)
+  ;(assoc tests/noop-test
+         ;:name    "ceph"
+         ;:os      debian/os
          ;;:db      (db version)
          ;;:client  (client nil nil)
-         :nemesis (nemesis/partition-random-halves)
-         :generator (->> (gen/mix [r w])
-                         (gen/stagger 1)
-                         (gen/nemesis
-                           (gen/seq (cycle [(gen/sleep 5)
-                                            {:type :info, :f :start}
-                                            (gen/sleep 5)
-                                            {:type :info, :f :stop}])))
-                         (gen/time-limit 60))
+         ;:nemesis (nemesis/partition-random-halves)
+         ;:generator (->> (gen/mix [r w])
+                         ;(gen/stagger 1)
+                         ;(gen/nemesis
+                           ;(gen/seq (cycle [(gen/sleep 5)
+                                            ;{:type :info, :f :start}
+                                            ;(gen/sleep 5)
+                                            ;{:type :info, :f :stop}])))
+                         ;(gen/time-limit 60))
          ;;:model   (model/set)
-         :checker (checker/compose
-                    {:perf   (checker/perf)
-                     :linear checker/linearizable})))
+         ;:checker (checker/compose
+                    ;{:perf   (checker/perf)
+                     ;:linear checker/linearizable})))
