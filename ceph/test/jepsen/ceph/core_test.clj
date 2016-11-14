7137837f963fc74c97837bed5747f7f9fed18e9c
Yiming Zhang <steven.y.zh@hotmail.com>
2016-11-14 10:36:28 -0800

    test

--- ceph/test/jepsen/ceph/core_test.clj
+++ ceph/test/jepsen/ceph/core_test.clj
@@ -1,8 +1,11 @@
 (ns ceph.core-test
   (:require [clojure.test :refer :all]
             ;;[jepsen.ceph.core :refer :all]))
-            [jepsen.ceph.core :as jc]))
+            [jepsen.ceph.core :as ceph]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;;(deftest a-test
+  ;;(testing "FIXME, I fail."
+    ;;(is (= 0 1))))
+
+(deftest ceph-test
+  (is (:valid? (:results (jepsen/run! (ceph/ceph-test "3.4.5+dfsg-2"))))))
