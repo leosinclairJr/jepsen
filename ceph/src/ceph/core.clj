8a36d9d19a863707ae381a1577d4b16d6434fde5
Yiming Zhang <steven.y.zh@hotmail.com>
2016-11-14 08:59:01 -0800

    commented out some lines

--- ceph/src/ceph/core.clj
+++ ceph/src/ceph/core.clj
@@ -2,14 +2,14 @@
   (:require [clojure.tools.logging :refer :all]
             [clojure.java.io    :as io]
             [clojure.string     :as str]
-            [jepsen [db         :as db]
+            [jepsen ;;[db         :as db]
                     [checker    :as checker]
-                    [client     :as client]
+                    ;;[client     :as client]
                     [control    :as c]
                     [generator  :as gen]
                     [nemesis    :as nemesis]
                     [tests      :as tests]
-                    [util       :refer [timeout]]]
+                    ;;[util       :refer [timeout]]]
             [jepsen.os.debian   :as debian]
             [knossos.model      :as model]))
 
