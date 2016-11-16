20d5783510fbe1b6cc0641cb6ae712112123664d
Yiming Zhang <steven.y.zh@hotmail.com>
2016-11-16 08:34:06 -0800

    test block comment

--- ceph/src/jepsen/ceph/core.clj
+++ ceph/src/jepsen/ceph/core.clj
@@ -72,24 +72,25 @@
   ;(println "Working!")
   ;tests/noop-test
   ;)
-
-;(defn ceph-test
-  ;[version]
-  ;(assoc tests/noop-test
-         ;:name    "ceph"
-         ;:os      debian/os
-         ;;:db      (db version)
-         ;;:client  (client nil nil)
-         ;:nemesis (nemesis/partition-random-halves)
-         ;:generator (->> (gen/mix [r w])
-                         ;(gen/stagger 1)
-                         ;(gen/nemesis
-                           ;(gen/seq (cycle [(gen/sleep 5)
-                                            ;{:type :info, :f :start}
-                                            ;(gen/sleep 5)
-                                            ;{:type :info, :f :stop}])))
-                         ;(gen/time-limit 60))
-         ;;:model   (model/set)
-         ;:checker (checker/compose
-                    ;{:perf   (checker/perf)
-                     ;:linear checker/linearizable})))
+(comment
+(defn ceph-test
+  [version]
+  (assoc tests/noop-test
+         :name    "ceph"
+         :os      debian/os
+         ;:db      (db version)
+         ;:client  (client nil nil)
+         :nemesis (nemesis/partition-random-halves)
+         :generator (->> (gen/mix [r w])
+                         (gen/stagger 1)
+                         (gen/nemesis
+                           (gen/seq (cycle [(gen/sleep 5)
+                                            {:type :info, :f :start}
+                                            (gen/sleep 5)
+                                            {:type :info, :f :stop}])))
+                         (gen/time-limit 60))
+         ;:model   (model/set)
+         :checker (checker/compose
+                    {:perf   (checker/perf)
+                     :linear checker/linearizable})))
+)
